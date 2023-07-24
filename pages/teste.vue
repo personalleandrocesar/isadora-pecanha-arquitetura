8eb4ea61b96daf9d7638a7c53d4f2dffc0e273da
professorleandrocesar <professor@leandrocesar.co>
2023-07-24 19:29:21 -0300

    updt animation

--- pages/teste.vue
+++ pages/teste.vue
@@ -1,33 +1,12 @@
+<script setup lang="ts">
+useHead({
+    titleTemplate: 'Isadora Peçanha - Arquitetura',
+})
+</script>
 
 <template>
-
-
-
-      <main class="main-content1">
-        <section class="main-content-section-title">
-          <h2>Índice de Massa Corporal (IMC)</h2>
-        </section>
-        <section class="main-content-section-principal">
-          <p>Valor e Classificação</p>
-          <p>Digite sua Massa (kg):</p>
-          <label>
-            <input type="text" class="div1_imc" placeholder="Digite só o valor (83.5)">
-          </label>
-          <br>
-          <br>
-          <p>Digite sua altura (m):</p>
-          <label>
-            <input type="text" class="div2_imc" placeholder="Digite só o valor (1.80)">
-          </label>
-          <br>
-          <button class="buttonCalc calculate-imc">Calcular IMC</button>
-          <br>
-          <br>
-          <div id="outputOne"></div>
-          <br>
-          </section>
-
-      </main>
-
-
+    <main>
+        <LogoHome />
+        <TheWelcome />
+    </main>
 </template>
